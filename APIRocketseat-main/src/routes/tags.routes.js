b0e35e07b4b3de0c62585b74ffd094845db23930
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-08-16 20:02:58 -0300

    finalizado bloco1 do stage 10

--- APIRocketseat-main/src/routes/tags.routes.js
+++ APIRocketseat-main/src/routes/tags.routes.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
 
 const TagsController = require("../controllers/TagsController");
+const ensureAuthenticaded = require("../middlewares/ensureAuthenticated");
+
 
 const tagsRoutes = Router();
 
 const tagsController = new TagsController();  
 
-tagsRoutes.get("/:user_id", tagsController.index);
+tagsRoutes.get("/", ensureAuthenticaded, tagsController.index);
 
 
 module.exports = tagsRoutes; 
