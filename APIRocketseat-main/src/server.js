2d839d5cae64adfb363dac34daaa9122347de167
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-08-20 09:30:03 -0300

    tentando resolver problema que deu pra pegar informações na base de dados

--- APIRocketseat-main/src/server.js
+++ APIRocketseat-main/src/server.js
@@ -3,12 +3,14 @@ const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError");
 const uploadConfig = require("./configs/upload");
 
+
 const express = require('express'); //aqui importa os módulos da pasta express em node_modules
 const routes = require("./routes");
 
 migrationsRun(); 
 
 const app = express(); //aqui faz a execução do express
+
 app.use(express.json()); //informa que o conteúdo vai ser no formato JSON
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
